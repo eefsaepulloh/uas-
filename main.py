31f2d8ca0c5e11c0577419873495b8a288ab3be9
eefsaepulloh <eefsaepulloh11@gmail.com>
2020-01-19 09:57:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from core.baseapp import BaseApp
+from view.view_book import import *
+
+class MainApp():
+    def __init__(self):
+        self.books = []
+
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
+
